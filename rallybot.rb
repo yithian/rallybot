5511fc56a1b4dbaf37f8718ffc79af8ef9056860
Alex Chvatal <achvatal@redhat.com>
2014-09-19 16:30:23 -0400

    ok maybe they can be symbols but ugh
    for #3 and #5

--- rallybot.rb
+++ rallybot.rb
@@ -7,8 +7,7 @@ require 'rally_api'
 require_relative './utils.rb'
 
 $items = {stories: :story, tasks: :task, defects: :defect}
-# ruby doesn't allow for '-' in symbols, so in-progres and In-Progress get to be strings
-$states = {backlog: :Backlog, defined: :Defined, 'in-progress' => 'In-Progress', completed: :Completed, accepted: :Accepted}
+$states = {backlog: :Backlog, defined: :Defined, :'in-progress' => :'In-Progress', completed: :Completed, accepted: :Accepted}
 
 bot = Cinch::Bot.new do
   configure do |c|
